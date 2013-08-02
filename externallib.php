f9f8ddd83791983edf50056b44b2f2b84176e782
fabioariati <fabiocariati@gmail.com>
2013-08-02 17:15:24 -0300

    Função para criar item de nota

--- externallib.php
+++ externallib.php
@@ -128,16 +128,25 @@ class local_wstcc_external extends external_api {
      *
      * @return array
      */
-    public static function create_grade_item() {
-        return array('test' => 'testando');
+    public static function create_grade_item($courseid, $itemname, $grademin, $grademax) {
+        $params = array(
+            'itemname'=>$itemname,
+            'grademin'=>$grademin,
+            'grademax'=>$grademax
+        );
+        $result = grade_update('local/wstcc', $courseid, 'local', 'wstcc', null, 0, null, $params);
+        if($result == 0) {
+            return array('result' => 'update successful');
+        } else {
+            return array('result' => 'error saving');
+        }
     }
 
     public static function create_grade_item_parameters() {
         return new external_function_parameters(
             array(
-                'userid' => new external_value(PARAM_INT, 'User id', VALUE_REQUIRED),
                 'courseid' => new external_value(PARAM_INT, 'Course id', VALUE_REQUIRED),
-                'itemname' => new external_value(PARAM_, 'Item Name', VALUE_REQUIRED),
+                'itemname' => new external_value(PARAM_RAW, 'Item Name', VALUE_REQUIRED),
                 'grademin' => new external_value(PARAM_INT, 'Grade min', VALUE_REQUIRED),
                 'grademax' => new external_value(PARAM_INT, 'Grade max', VALUE_REQUIRED)
             )
@@ -146,9 +155,9 @@ class local_wstcc_external extends external_api {
 
     public static function create_grade_item_returns() {
         $keys = array(
-            'test' => new external_value(PARAM_RAW, 'test'),
+            'result' => new external_value(PARAM_RAW, 'result'),
         );
 
-        return new external_single_structure($keys, 'Teste.');
+        return new external_single_structure($keys, 'Result.');
     }
 }
